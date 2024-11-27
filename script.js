1927072fa7806039c8c560cc71a42ab2c41f9b0d
“akshaykumar401” <akshaykumar835103@gmail.com>
2024-11-27 20:02:07 +0530

    final KeyPress Update

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ const body = document.querySelector(".body");
 // Random Number Genrator..
 const randNumber = Math.floor(Math.random() * 101);
 
-btn.addEventListener("click", (e) => {
+// congurational Flag Function Defination.
+function congurational(){
+    let node = document.createElement("img");
+    node.src = "./src/win reb1.gif";
+    node.style.position = "absolute";
+    node.style.width = "100dvw";
+    node.style.height = "100dvh";
+    node.style.zIndex = "-50";
+        
+    body.appendChild(node);
+}
+
+function cheakVal(e){
     e.preventDefault();
 
     if(Number(input.value) === randNumber){
@@ -22,17 +34,9 @@ btn.addEventListener("click", (e) => {
         btn.style.width = "100%";
         btn.innerText = "Reset";
         btn.style.borderRadius = "20px";
-        let node = document.createElement("img");
-        node.src = "./src/win reb1.gif";
-        node.style.position = "absolute";
-        node.style.width = "100dvw";
-        node.style.height = "100dvh";
-        node.style.zIndex = "-50";
-        
-        body.appendChild(node);
-
-
-
+        // congurational Flag Function Calling
+        congurational();
+        // Event Listner For Reset Button
         btn.addEventListener('click', (e) => {
             location.reload();
         })
@@ -43,4 +47,15 @@ btn.addEventListener("click", (e) => {
         msg.innerText = "Too Low!";
         refImage.src = "./src/No.gif";
     }
+}
+
+// Input Event Listenor for cheakin input...
+input.addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+        cheakVal(e)
+    }
 });
+
+
+// Button Event Listenor...
+btn.addEventListener("click", cheakVal);
